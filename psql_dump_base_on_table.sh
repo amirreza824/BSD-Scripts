683d2f7ae304346974931ee990980ce2f10978a1
AmirReza Hashemi <amirreza824@gmail.com>
2017-08-07 11:03:45 +0430

    change psql dump script

--- psql_dump_base_on_table.sh
+++ psql_dump_base_on_table.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
-sudo -i -u pgsql psql ipdr -c "\dt" | grep -i "route" | cut -f 2 -d "|" |  while read line ; do sudo -i -u pgsql pg_dump --no-owner --no-tablespaces --encoding=utf8 --username=pgsql 
--t ${line} ipdr > /usr/home/amir/pg_dump_${line}.sql ; done
-sudo -i -u pgsql psql ipdr -c "\dt" | grep -i "udr" | cut -f 2 -d "|" |  while read line ; do sudo -i -u pgsql pg_dump --no-owner --no-tablespaces --encoding=utf8 --username=pgsql -t 
-${line} ipdr > /usr/home/amir/pg_dump_${line}.sql ; done
-sudo -i -u pgsql psql ipdr -c "\dt" | grep -i "xdr" | cut -f 2 -d "|" |  while read line ; do sudo -i -u pgsql pg_dump --no-owner --no-tablespaces --encoding=utf8 --username=pgsql -t 
-${line} ipdr > /usr/home/amir/pg_dump_${line}.sql ; done
+DIR="/usr/home/amir/"  ## Directory that you want store dumped file
 
+psql_user=`cat /etc/passwd | grep -i PostgreSQL | cut -f1 -d ":"`
+psql_db="ipdr"
+psql_app=`which psql`
+psql_dump=`which pg_dump`
+sudo_app=`which sudo`
+
+table_1="route"
+table_2="udr"
+table_3="xdr"
+
+cd ${DIR} ## Change Directory to Destination fo Backupng
+
+## Table 1
+${sudo_app} -i -u ${psql_user} ${psql_app} ${psql_db} -c "\dt" | grep -i "${table_1}" | cut -f 2 -d "|" |  while read line
+  do
+    ${sudo_app} -i -u ${psql_user} ${psql_dump} --no-owner --no-tablespaces --encoding=utf8 --username=${psql_user} -t ${line} ipdr > ${DIR}pg_dump_${line}.sql
+  done
+
+## Table 2
+${sudo_app} -i -u ${psql_user} ${psql_app} ${psql_db} -c "\dt" | grep -i "${table_2}" | cut -f 2 -d "|" |  while read line
+  do
+    ${sudo_app} -i -u ${psql_user} ${psql_dump} --no-owner --no-tablespaces --encoding=utf8 --username=${psql_user} -t ${line} ipdr > ${DIR}pg_dump_${line}.sql
+  done
+
+## Table 3
+${sudo_app} -i -u ${psql_user} ${psql_app} ${psql_db} -c "\dt" | grep -i "${table_3}" | cut -f 2 -d "|" |  while read line
+  do
+    ${sudo_app} -i -u ${psql_user} ${psql_dump} --no-owner --no-tablespaces --encoding=utf8 --username=${psql_user} -t ${line} ipdr > ${DIR}pg_dump_${line}.sql
+  done
+
+###################################
